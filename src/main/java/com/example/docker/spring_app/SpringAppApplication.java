ced7618d53a8a97f698b8002f52b94722493a1c9
Ubuntu <ubuntu@ip-172-31-0-35.ap-south-1.compute.internal>
2024-12-20 17:17:02 +0000

    Created Java app and updated all files

---
+++ src/main/java/com/example/docker/spring_app/SpringAppApplication.java
@@ -0,0 +1,26 @@
+package com.example.docker.spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAppApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Dockerizing Spring Boot Application";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "<h1>This is where you will see the login page</h1>";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppApplication.class, args);
+    }
+}
+
