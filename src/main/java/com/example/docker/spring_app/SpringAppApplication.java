20cd3da7aa3eb26e1d71c51a4f7e2a0327313c0a
freelanceagdevops <freelanceagdevops@gmail.com>
2024-12-23 01:04:22 +0530

    Update SpringAppApplication.java

--- src/main/java/com/example/docker/spring_app/SpringAppApplication.java
+++ src/main/java/com/example/docker/spring_app/SpringAppApplication.java
@@ -5,29 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Main application class for the Spring Boot application.
- * This class is not designed for extension.
- */
 @SpringBootApplication
 @RestController
-public final class SpringAppApplication { // Marked as final to prevent subclassing
+public class SpringAppApplication {
 
-    /**
-     * Handles the root URL ("/") and returns a greeting message.
-     * 
-     * @return a greeting message
-     */
     @RequestMapping("/")
     public String home() {
         return "Dockerizing Spring Boot Application";
     }
 
-    /**
-     * Handles the "/login" URL and returns a message for the login page.
-     * 
-     * @return a message for the login page
-     */
     @RequestMapping("/login")
     public String login() {
         return "<h1>This is where you will see the login page</h1>";
