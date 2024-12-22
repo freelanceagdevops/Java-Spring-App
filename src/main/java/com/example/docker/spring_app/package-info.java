b4402906bc90958d80e105036bcc5495f8d9dd62
freelanceagdevops <freelanceagdevops@gmail.com>
2024-12-23 00:12:14 +0530

    Create package-info.java

---
+++ src/main/java/com/example/docker/spring_app/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains the main application for the Spring app.
+ * It contains various classes that manage the application's functionality.
+ */
+package com.example.docker.spring_app;
